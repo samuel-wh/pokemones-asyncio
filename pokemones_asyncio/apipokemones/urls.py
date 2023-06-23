72faef869de1ba7485167ecab68c2c5be298d5c2
samuel-wh <samuel@wisphub.net>
2023-06-23 17:23:19 -0500

    Revert "practica async"
    This reverts commit 70bda80d4fad94a73ec0f1fe73fcbeb0721202ea.

--- pokemones_asyncio/apipokemones/urls.py
+++ pokemones_asyncio/apipokemones/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from .views.asyncio import pokemon_list_async
 from .views.original import pokemon_list
 
 urlpatterns = [
@@ -9,5 +8,4 @@ urlpatterns = [
 
     #   listar
     path('lista-original/', pokemon_list, name="pokemon_lista_original"),
-    path('lista-async/', pokemon_list_async, name="pokemon_lista_async"),
 ]
